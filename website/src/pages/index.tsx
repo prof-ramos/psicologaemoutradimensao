7236e757cdddfd36eba98d3d59c506b326366ce8
Gabriel Ramos <gabrielramos@Gabriels-MacBook-Air.local>
2025-09-21 18:52:08 -0300

    feat: Add coming soon page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,44 +1,18 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import React from 'react';
+import Head from '@docusaurus/Head';
+import ComingSoonButton from '@site/src/components/ComingSoonButton';
+import '../css/ComingSoon.css';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+export default function Home(): JSX.Element {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+    <>
+      <Head>
+        <title>Em Breve</title>
+        <meta name="description" content="Em Breve" />
+      </Head>
+      <div className="coming-soon-container">
+        <ComingSoonButton />
       </div>
-    </header>
-  );
-}
-
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    </>
   );
 }
